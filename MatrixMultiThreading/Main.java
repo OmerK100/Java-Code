e4dec73aaa498d4e5561a101b056b691fae590be
Omer Komissarchik <139342166+OmerK100@users.noreply.github.com>
2023-08-02 12:19:19 +0300

    Update Main.java

--- MatrixMultiThreading/Main.java
+++ MatrixMultiThreading/Main.java
@@ -1,4 +1,4 @@
-/* Advanced Java - MAMAN15EX1 - Omer Komissarchik - 215314725 */
+/* Matrix product calculator with input output and multithreading - Omer Komissarchik - 215314725 */
 /* Main class runs the matrix multi-threading simulation through user input. */
 import java.util.Scanner;
 
